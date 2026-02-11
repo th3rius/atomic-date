897eedd4a23de9bd7b6bb39f81987007f6f56d40
Gabriel de Oliveira <gabrielhk3@gmail.com>
2026-02-11 19:15:23 -0300

    use valueOf instead of Symbol.toPrimitive to match Date's type signature

--- src/atomic-date.js
+++ src/atomic-date.js
@@ -1,20 +1,16 @@
 export class AtomicDate extends Date {
-  [Symbol.toPrimitive](hint) {
-    if (hint === "number" || hint === "default") {
-      if (isNaN(this.getTime())) {
-        return NaN;
-      }
-
-      const year = String(this.getUTCFullYear());
-      const month = String(
-        // Months are zero-indexed
-        this.getUTCMonth() + 1,
-      ).padStart(2, "0");
-      const day = String(this.getUTCDate()).padStart(2, "0");
-      return Number(year + month + day);
+  valueOf() {
+    if (isNaN(this.getTime())) {
+      return NaN;
     }
 
-    return super[Symbol.toPrimitive](hint);
+    const year = String(this.getUTCFullYear());
+    const month = String(
+      // Months are zero-indexed
+      this.getUTCMonth() + 1,
+    ).padStart(2, "0");
+    const day = String(this.getUTCDate()).padStart(2, "0");
+    return Number(year + month + day);
   }
 
   toDate() {
--- src/atomic-date.spec.js
+++ src/atomic-date.spec.js
@@ -5,7 +5,7 @@ import { AtomicDate } from "./atomic-date.js";
 describe("AtomicDate", () => {
   it("should be represented as a number correctly", () => {
     const atomicDate = new AtomicDate("2026-01-11");
-    assert.equal(atomicDate, 20260111);
+    assert.strictEqual(Number(atomicDate), 20260111);
   });
 
   it("should be represented as NaN when date is invalid", () => {
@@ -18,6 +18,6 @@ describe("AtomicDate", () => {
   it("should convert back to a date", () => {
     const atomicDate = new AtomicDate("2026-02-11T20:40:29.565Z");
     const date = atomicDate.toDate();
-    assert.strictEqual(date.getTime(), 1770842429565);
+    assert.strictEqual(Number(date), 1770842429565);
   });
 });
